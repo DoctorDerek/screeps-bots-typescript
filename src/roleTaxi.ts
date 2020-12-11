d5f31a02dbda0bf4adcd8879dd16a378f6017d9e
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-12-10 22:10:22 -0500

    Add edge check before clearing taxi memory

--- src/roleTaxi.ts
+++ src/roleTaxi.ts
@@ -142,6 +142,8 @@ export const actionTaxi = (taxi: Creep) => {
       )
     },
   })
+  const taxiIsOnEdge =
+    taxi.pos.x > 0 && taxi.pos.x < 49 && taxi.pos.y > 0 && taxi.pos.y < 49
   if (target && target.memory.taxiDriver === taxi.name) {
     // This taxi is the assigned driver for that creep, let's go tow
     DEBUG && console.log(`${taxi.name} is trying to tow ${target.name}`)
@@ -150,12 +152,7 @@ export const actionTaxi = (taxi: Creep) => {
     const pullResult = taxi.pull(target)
 
     // normal code (not on edges)
-    if (
-      taxi.pos.x > 0 &&
-      taxi.pos.x < 49 &&
-      taxi.pos.y > 0 &&
-      taxi.pos.y < 49
-    ) {
+    if (taxiIsOnEdge) {
       // we're not on an edge, so go pickup our ride
       if (pullResult === ERR_NOT_IN_RANGE) {
         taxi.moveTo(target) // pickup ride
@@ -223,9 +220,16 @@ export const actionTaxi = (taxi: Creep) => {
     // Tigga from https://screeps.com/forum/topic/2808/pull-across-room-boundaries
     */
   } else {
-    // We're not working right now, so let's be sure our taxi
-    // light is on with the empty string saved as taxiDriver
-    taxi.memory.taxiDriver = ""
+    if (!taxiIsOnEdge) {
+      // We're not working right now, so let's be sure our taxi
+      // light is on with the empty string saved as taxiDriver
+      taxi.memory.taxiDriver = ""
+    } else {
+      // There's a chance we're transiting between rooms
+      console.log(
+        `${taxi.name} has taxiDriver in memory of ${taxi.memory.taxiDriver}`
+      )
+    }
   }
 }
 
